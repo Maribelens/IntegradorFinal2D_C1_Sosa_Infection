429ba2eaa25a8dcd2f3a63ecacd962c2603ef257
Maribelens <maribelensosa@gmail.com>
2026-01-22 18:06:34 -0300

    [Add] PlayerController with 360° movement (WASD)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // 1. Capturar input
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        // 2. Normalizar vector
+        movement = new Vector2(moveX, moveY).normalized;
+    }
+
+    void FixedUpdate()
+    {
+        // 3. Aplicar movimiento con Rigidbody2D
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
+
+
