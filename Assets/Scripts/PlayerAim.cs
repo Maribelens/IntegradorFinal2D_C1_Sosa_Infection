e56df7eb0093c75d7bf75f3c882ed5bf1aec1c92
Maribelens <maribelensosa@gmail.com>
2026-01-24 09:29:08 -0300

    [Add] Mouse aiming system

---
+++ Assets/Scripts/PlayerAim.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePos - transform.position;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+}
